3cec5e2876e2b526cbcae7f66ef3c87d67f05394
Ronaltex <ronaltex@yori-tex.com>
2026-07-30 00:13:04 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yoritex-v22-20260716-sync-sin-bloqueo';
+const CACHE_NAME = 'yoritex-v23-20260730-integridad-reposiciones';
 const APP_SHELL = ['./', './index.html', './manifest.json', './logo-yori-tex-plano.png'];
 const FIREBASE_CONFIG = {
   apiKey: 'AIzaSyA58LjJd3R28quWhTDlU4TXHVxK5e6eIxg',
