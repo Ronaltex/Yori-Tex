7f2ff9e8cb9d58e8552a373aa170170a5d26e18a
Ronaltex <ronaltex@yori-tex.com>
2026-07-31 16:56:39 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yoritex-v26-20260730-relaciones-repuestas';
+const CACHE_NAME = 'yoritex-v27-20260731-editor-inventario';
 const APP_SHELL = ['./', './index.html', './manifest.json', './logo-yori-tex-plano.png'];
 const FIREBASE_CONFIG = {
   apiKey: 'AIzaSyA58LjJd3R28quWhTDlU4TXHVxK5e6eIxg',
