fca609ca3be7b2346a0757e2a3173c32deba51ec
Ronaltex <ronaltex@yori-tex.com>
2026-09-01 02:22:46 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yoritex-v31-20260817-reposicion-cuenta-renovable';
+const CACHE_NAME = 'yoritex-v32-20260901-renovacion-cupo-nube';
 const APP_SHELL = ['./', './index.html', './manifest.json', './logo-yori-tex-plano.png'];
 const FIREBASE_CONFIG = {
   apiKey: 'AIzaSyA58LjJd3R28quWhTDlU4TXHVxK5e6eIxg',
