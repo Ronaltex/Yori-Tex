be976a58b5f2a1f2a501c7b64ca4a7fab1198f1b
Ronaltex <ronaltex@yori-tex.com>
2026-09-02 01:47:00 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yoritex-v35-20260902-responsive-ventas';
+const CACHE_NAME = 'yoritex-v36-20260902-garantias-por-producto';
 const APP_SHELL = ['./', './index.html', './manifest.json', './logo-yori-tex-plano.png'];
 const FIREBASE_CONFIG = {
   apiKey: 'AIzaSyA58LjJd3R28quWhTDlU4TXHVxK5e6eIxg',
